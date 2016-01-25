9bd5d463bf2103f4488ecd325d0b948b9694d982
ANDRE SHONUBI <andre@codedre.com>
2016-01-25 17:55:17 -0500

    added back readme

--- server/config/passport.js
+++ server/config/passport.js
@@ -14,63 +14,8 @@ module.exports = function(passport) {
       callback(err, user);
     });
   });
-  // passport.serializeUser(User.serializeUser());
-  // passport.deserializeUser(User.deserializeUser());
-  passport.use(new LocalStrategy(User.authenticate()));
 
-  // passport.use("local-signup", new LocalStrategy({
-  //   username : "email",
-  //   password : "password",
-  //   passReqToCallback : true
-  // }, function(req, email, password, callback){
-  //   // Find a user with this e-mail
-  //     User.findOne({ "local.email" : email }, function(err, user) {
-  //       if (err) return callback(err);
-  //
-  //       // If there already is a user with this email
-  //       if (user) {
-  //         return callback(null, false, req.flash("signupMessage", "This email is already used."));
-  //       } else {
-  //         // There is no email registered with this email
-  //
-  //         // Create a new user
-  //         var newUser = new User();
-  //         newUser.local.email = email;
-  //         newUser.local.password = newUser.encrypt(password);
-  //
-  //         newUser.save(function(err) {
-  //           if (err) throw err;
-  //           return callback(null, newUser);
-  //         });
-  //       }
-  //   });
-  // }));
-  //
-  // passport.use("local-login", new LocalStrategy({
-  //   usernameField : "email",
-  //   passwordField : "password",
-  //   passReqToCallback : true
-  // }, function(req, email, password, callback){
-  //
-  //   // Search for a user with this email
-  //   User.findOne({ "local.email" : email }, function(err, user) {
-  //     if (err) {
-  //       return callback(err);
-  //     }
-  //
-  //     // If no user is found
-  //     if (!user) {
-  //       return callback(null, false, req.flash("loginMessage", "No user found."));
-  //     }
-  //
-  //     // Wrong password
-  //     if (!user.validPassword(password)) {
-  //       return callback(null, false, req.flash("loginMessage", "Oops! Wrong password."));
-  //     }
-  //
-  //     return callback(null, user);
-  //   });
-  // }));
+  passport.use(new LocalStrategy(User.authenticate()));
 
   passport.use(new FacebookStrategy({
 
